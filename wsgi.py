7b6ddbbc65e5e4eabb632000d4f05c9d1002d377
YokaiEmporer <taranrishit1234@gmail.com>
2020-04-02 01:23:51 +0530

    awfawvAWFV

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 from plotdt import app
 from waitress import serve
+import os
+port = int(os.environ.get('PORT', 5000))
 if __name__ == "__main__":
     #app.run()
-    serve(app)
+    serve(app, host='0.0.0.0', port=port)
