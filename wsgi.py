af22a370b0eb2cc8fdcdde075bd7e0aef1266d9e
YokaiEmporer <taranrishit1234@gmail.com>
2020-04-03 04:18:47 +0530

    awf

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 from plotdt import app
-from waitress import serve
+# import os
+# os.system("pip install waitress")
+# from waitress import serve
 import os
 port = int(os.environ.get('PORT', 5000))
 if __name__ == "__main__":
-    #app.run()
-    serve(app, host='0.0.0.0', port=port)
+    app.run()
+    # serve(app, host='0.0.0.0', port=port)
